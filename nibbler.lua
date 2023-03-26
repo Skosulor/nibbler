4922d11c09343539e46e301377091d4ba944b01d
Andreas Öhman <andreas.ohman@>
2023-03-26 22:42:16 +0200

    Toggle base

--- nibbler.lua
+++ nibbler.lua
@@ -1,10 +1,12 @@
 local api = vim.api
 local Nibbler = {}
 
+
 local function get_word_under_cursor()
   local cword = vim.fn.expand('<cword>')
   return cword
 end
+
 
 local function replace_word_under_cursor(new_word)
   local current_line = api.nvim_get_current_line()
@@ -12,6 +14,7 @@ local function replace_word_under_cursor(new_word)
   local replaced_line = vim.fn.substitute(current_line, old_word, new_word, '')
   api.nvim_set_current_line(replaced_line)
 end
+
 
 function Nibbler.convert_to_hex()
   local word = get_word_under_cursor()
@@ -26,6 +29,7 @@ function Nibbler.convert_to_hex()
   end
 end
 
+
 local function to_binary(number)
   local binary = ""
   while number > 0 do
@@ -34,6 +38,7 @@ local function to_binary(number)
   end
   return '0b' .. binary
 end
+
 
 function Nibbler.convert_to_binary()
   local word = get_word_under_cursor()
@@ -45,6 +50,7 @@ function Nibbler.convert_to_binary()
     end
   end
 end
+
 
 function Nibbler.convert_to_decimal()
   local word = get_word_under_cursor()
@@ -63,6 +69,34 @@ function Nibbler.convert_to_decimal()
     end
   end
 end
+
+
+function Nibbler.toggle_base()
+  local word = get_word_under_cursor()
+  if word then
+    local number
+
+    if string.match(word, '^0b') then
+      number = tonumber(word:sub(3), 2)
+      if number then
+        replace_word_under_cursor(tostring(number))
+      end
+    elseif string.match(word, '^0x') then
+      number = tonumber(word, 16)
+      if number then
+        local binary_number = to_binary(number)
+        replace_word_under_cursor(binary_number)
+      end
+    else
+      number = tonumber(word)
+      if number then
+        local hex_number = string.format('%#x', number)
+        replace_word_under_cursor(hex_number)
+      end
+    end
+  end
+end
+
 
 return Nibbler
 
