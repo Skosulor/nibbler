267be9ba5bc3a783535e8646442b71eadd12ebfb
Andreas Öhman <andreas.ohman@>
2023-03-26 22:28:40 +0200

    Replace word

--- nibbler.lua
+++ nibbler.lua
@@ -6,4 +6,12 @@ local function get_word_under_cursor()
   return cword
 end
 
+local function replace_word_under_cursor(new_word)
+  local current_line = api.nvim_get_current_line()
+  local old_word = vim.fn.expand('<cword>')
+  local replaced_line = vim.fn.substitute(current_line, old_word, new_word, '')
+  api.nvim_set_current_line(replaced_line)
+end
+
+function Nibbler.convert_to_hex()
 return Nibbler
